288fcd3472c09d7179787443e2ecf2e4354f0a2f
Juan Miguel  Arias Mejias <juanmiguelar09@gmail.com>
2025-07-17 14:31:00 -0600

    fix vuetify path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue(), vuetify({ autoImport: true })],
-  server: {
-    port: 3000,
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'vuetify/lib': resolve(__dirname, 'node_modules/vuetify/lib'),
+    },
   },
+  server: { port: 3000 },
 })
