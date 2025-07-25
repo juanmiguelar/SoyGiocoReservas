ecd759e9350dbb967c2bb002cb56b93558f716d3
Juan Miguel  Arias Mejias <juanmiguelar09@gmail.com>
2025-07-25 11:38:08 -0600

    Add MDI icons support

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,21 @@ import router from './router'
 import { useGoogleAuthStore } from './stores/googleAuth'
 
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
-const vuetify = createVuetify({ components, directives })
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: { mdi },
+  },
+})
 
 const app = createApp(App)
 const pinia = createPinia()
